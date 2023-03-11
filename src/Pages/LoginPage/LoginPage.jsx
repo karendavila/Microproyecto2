89114c53cdc8ef721a6e5382fbac47e88cd894f7
Karen Davila <karen.davila@correo.unimet.edu.ve>
2023-03-11 13:23:56 -0400

    create loginpage
    login html y css

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import "./LoginPage.css";
+
+
 
 export function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
-}
+
+
+    return (
+      <div className='Container'>
+        <form>
+  
+          <div className='bloque1'>
+              <h1 className='titulo'>¡Bienvenido!</h1>
+          </div>
+  
+          <h3 className='titulo2'>Iniciar sesión</h3>
+  
+          <div className='rectangulol'></div>
+  
+          <h5 className='subtitulo1' id='sub1'>Email</h5>
+
+
+          
+
+            <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            className="search-field1"></input>
+    
+            <h5 className='subtitulo2'>Contraseña</h5>
+    
+            <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="*******" 
+            className="search-field2" 
+            ></input>
+    
+            <button 
+            type="submit" 
+            className="button1L">Iniciar sesión</button>
+    
+          
+          
+
+
+            <button type="button" className="button2L" >Continuar con Google</button>
+    
+
+          <img class="logoGoogleL" src="img/google.png" alt="" />
+  
+   
+          <h5 className='titulo4'>Todos los derechos reservados</h5>
+  
+        </form>
+      </div>
+  
+      
+    )
+  }
+  
+  export default LoginPage;
